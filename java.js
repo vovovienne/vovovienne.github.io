120a6c960e2ca46776e3364d4eb3cb0530af5cef
vovovienne <vvivienne.lam@gmail.com>
2024-07-09 03:24:58 +1000

    reformatted site

--- java.js
+++ java.js
@@ -23,73 +23,30 @@ function playdisc() {
 }
 
 
-function showtext () {
-	document.getElementById('illust1desc').className = 'textreveal';
-}
-
-function hidetext () {
-	document.getElementById('illust1desc').className = 'illust1desc';
-}
-
-function showtext2 () {
-	document.getElementById('illust2desc').className = 'textreveal';
-}
-
-function hidetext2 () {
-	document.getElementById('illust2desc').className = 'illust2desc';
-}
-
-function showtext3 () {
-	document.getElementById('illust3desc').className = 'textreveal';
-}
-
-function hidetext3 () {
-	document.getElementById('illust3desc').className = 'illust3desc';
-}
-
-function showtext4 () {
-	document.getElementById('illust4desc').className = 'textreveal';
-}
-
-function hidetext4 () {
-	document.getElementById('illust4desc').className = 'illust4desc';
-}
-
-function showtext5 () {
-	document.getElementById('illust5desc').className = 'textreveal';
-}
-
-function hidetext5 () {
-	document.getElementById('illust5desc').className = 'illust5desc';
-}
-
-function showtext6 () {
-	document.getElementById('illust6desc').className = 'textreveal';
-}
-
-function hidetext6 () {
-	document.getElementById('illust6desc').className = 'illust6desc';
-}
-
-function showtext7 () {
-	document.getElementById('illust7desc').className = 'textreveal';
-}
-
-function hidetext7 () {
-	document.getElementById('illust7desc').className = 'illust7desc';
-}
-
-
-function showtext8 () {
-	document.getElementById('illust8desc').className = 'textreveal';
-}
-
-function hidetext8 () {
-	document.getElementById('illust8desc').className = 'illust8desc';
-}
-
-
 $(window).on("load", function(){
 	$(".loading").fadeOut("slow");
 	$("body").removeClass("preload");
 })
+
+
+
+function opensamplesite(iframeid, samplesitesrc) {
+    const divcont = document.getElementById("samplesitecont");
+    const alliframes = divcont.getElementsByTagName("iframe");
+    const iframe = document.getElementById(iframeid);
+  
+    const addsamplesite = document.createElement('iframe');
+    addsamplesite.src = (samplesitesrc);
+    addsamplesite.classList.add("samplesite");
+    addsamplesite.setAttribute("id", iframeid);
+
+    for (const ifr of alliframes) {
+      if ((ifr.id != iframeid) || (iframe != null)) {
+        divcont.removeChild(ifr);
+      }
+    }
+    
+    if (iframe == null) {
+      divcont.appendChild(addsamplesite);
+    }
+  }
